bdb2a425d5059885bd18a473a912d40a181ec1b2
glamorouslife <pardis.shariffar@gmail.com>
2024-06-04 20:31:13 +0200

    Add Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    splittedWord=word.split("")
+    console.log(splittedWord)
+    reversedSplittedWord=splittedWord.reverse()
+    console.log(reversedSplittedWord)
+    joinCharacters=reversedSplittedWord.join("")
+    console.log(joinCharacters)
+    return joinCharacters
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
